9e6eea2e95da76bf2a912ea4238861d2d08524e2
Aditi-Suresh-ada <aditi.suresh@ada.ac.uk>
2026-02-28 23:03:43 +0000

    staff management done

--- src/queries/7_staff_management.sql
+++ src/queries/7_staff_management.sql
@@ -2,7 +2,23 @@
 .mode column
 
 -- 7.1 
-
+SELECT
+    s.staff_id,
+    s.first_name,
+    s.last_name,
+    s.position AS role
+FROM staff AS s
+;
 
 -- 7.2 
-
+SELECT s.staff_id AS trainer_id,
+       CONCAT(s.first_name, ' ', s.last_name) AS full_name, 
+       COUNT(*) AS session_count
+FROM personal_training_sessions AS pts
+JOIN staff AS s
+  ON pts.staff_id = s.staff_id
+WHERE date(pts.session_date) BETWEEN date('2025-01-20')
+                               AND date('2025-01-20', '+30 days')
+GROUP BY s.staff_id, s.first_name, s.last_name
+HAVING COUNT(*) >= 1
+ORDER BY s.staff_id;
